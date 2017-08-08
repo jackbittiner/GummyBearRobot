7e5a23a4499fb198f8d7f612e1fd4ec1fd4fa92c
Jack Bittiner <jackbittiner@hotmail.com>
2017-08-08 14:14:40 +0100

    Warehouse has function that can add crates

--- spec/warehouseSpec.js
+++ spec/warehouseSpec.js
@@ -10,5 +10,17 @@ describe('Warehouse', function() {
       expect(warehouse.coordinates[0]).toEqual([-3, -3]);
       expect(warehouse.coordinates[48]).toEqual([3, 3]);
     });
+
+    it('is initialized with no crates in it', function() {
+      expect(warehouse.crates).toEqual([])
+    })
+  });
+
+  describe('addCrate', function() {
+    it('adds a crate to the warehouse with coordinates and number of gummybears', function() {
+      warehouse.addCrate(0,0,5);
+      expect(warehouse.crates.length).toEqual(1);
+      expect(warehouse.crates[0].gummybears.length).toEqual(5);
+    });
   });
 });
--- src/warehouse.js
+++ src/warehouse.js
@@ -1,12 +1,17 @@
 function Warehouse(x, x2, y, y2){
 	this.coordinates = [];
-  this.generateCoordinates(x,x2,y,y2)
+  this.generateCoordinates(x,x2,y,y2);
+  this.crates = [];
 }
 
 Warehouse.prototype.generateCoordinates = function(x,x2,y,y2) {
   for( var i = x; i <= x2; i++ ) {
       for( var j = y; j <= y2; j++ ) {
           this.coordinates.push([i,j]);
-      }
+    }
   }
+}
+
+Warehouse.prototype.addCrate = function(x, y, quantity) {
+  this.crates.push(new Crate(x, y, quantity));
 }
