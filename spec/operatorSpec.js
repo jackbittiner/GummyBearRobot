7963387ef7e1440ec91e407153130386f715e1f0
Jack Bittiner <jackbittiner@hotmail.com>
2017-08-08 18:23:33 +0100

    Added another crate to test to make sure it wouldnt deplete gummybears

--- spec/operatorSpec.js
+++ spec/operatorSpec.js
@@ -41,6 +41,7 @@ describe('Operator', function() {
 
     beforeEach(function() {
       operator.warehouse.addCrate(0, 0, 10);
+      operator.warehouse.addCrate(0, 1, 10);
     });
 
     it('adds one gummybear to the robots array when it picks up on a crates coordinates', function() {
@@ -51,6 +52,7 @@ describe('Operator', function() {
     it('subtracts one gummybear from the crates array when it picks up on a crates coordinates', function() {
       operator.makeRobotPickUp();
       expect(operator.warehouse.crates[0].gummybears.length).toEqual(9);
+      expect(operator.warehouse.crates[1].gummybears.length).toEqual(10);
     });
   });
 });
